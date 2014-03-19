872c3f8515d10214e7834d60e136a39938c1e135
Petre Patrasc <petre@dreamlabs.ro>
2014-03-19 15:26:40 +0200

    Improved the season entry parsing service - lowered complexity and added class comments.

--- Service/Parsing/Season/EntryParsingService.php
+++ Service/Parsing/Season/EntryParsingService.php
@@ -9,38 +9,76 @@ use petrepatrasc\BlizzardApiBundle\Service\Parsing\Ladder\InformationParsingServ
 use petrepatrasc\BlizzardApiBundle\Service\Parsing\Ladder\NonRankedParsingService;
 use petrepatrasc\BlizzardApiBundle\Service\Parsing\ParsingInterfaceStandalone;
 
+/**
+ * Handles parsing season entry data from the API.
+ * @package petrepatrasc\BlizzardApiBundle\Service\Parsing\Season
+ */
 class EntryParsingService implements ParsingInterfaceStandalone
 {
 
     /**
      * Extract season entry information from an array.
      *
-     * @param array $params
-     * @return Entry
+     * @param array $params The parameters with the API response where the data should be extracted from.
+     * @return Object containing season information.
      */
     public static function extract($params)
     {
         $seasonEntry = new Entry();
 
-        $ladderInformationArray = array();
-        foreach ($params['ladder'] as $ladderInformationEntry) {
-            $ladderInformationArray[] = InformationParsingService::extract($ladderInformationEntry);
-        }
-
-        $characters = array();
-        foreach ($params['characters'] as $character) {
-            $characters[] = BasicProfileParsingService::extract($character);
-        }
-
-        $nonRankedArray = array();
-        foreach ($params['nonRanked'] as $nonRankedEntry) {
-            $nonRankedArray[] = NonRankedParsingService::extract($nonRankedEntry);
-        }
+        $ladderInformationArray = self::extractLadderInformationData($params);
+        $characters = self::extractCharacterData($params);
+        $nonRankedArray = self::extractNonRankedLadderData($params);
 
         $seasonEntry->setLadderInformation($ladderInformationArray)
             ->setCharacters($characters)
             ->setNonRanked($nonRankedArray);
 
         return $seasonEntry;
+    }
+
+    /**
+     * Extract ladder information from an array.
+     *
+     * @param array $params The parameters with the API response where the data should be extracted from.
+     * @return array
+     */
+    public static function extractLadderInformationData($params)
+    {
+        $ladderInformationArray = array();
+        foreach ($params['ladder'] as $ladderInformationEntry) {
+            $ladderInformationArray[] = InformationParsingService::extract($ladderInformationEntry);
+        }
+        return $ladderInformationArray;
+    }
+
+    /**
+     * Extract basic profile data from an array.
+     *
+     * @param array $params The parameters with the API response where the data should be extracted from.
+     * @return array
+     */
+    public static function extractCharacterData($params)
+    {
+        $characters = array();
+        foreach ($params['characters'] as $character) {
+            $characters[] = BasicProfileParsingService::extract($character);
+        }
+        return $characters;
+    }
+
+    /**
+     * Extract non-ranked ladder information from an array.
+     *
+     * @param array $params The parameters with the API response where the data should be extracted from.
+     * @return array
+     */
+    public static function extractNonRankedLadderData($params)
+    {
+        $nonRankedArray = array();
+        foreach ($params['nonRanked'] as $nonRankedEntry) {
+            $nonRankedArray[] = NonRankedParsingService::extract($nonRankedEntry);
+        }
+        return $nonRankedArray;
     }
 }
